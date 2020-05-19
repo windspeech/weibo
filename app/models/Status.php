8bb4e526424db2b9537a2414ee057c41dde2ded5
zhuozhanyang <windzhuo@goatgames.com>
2020-05-19 19:52:35 +0800

    用户微博列表“

--- app/models/Status.php
+++ app/models/Status.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
     public function user()
     {
         return $this->belongsTo(User::class);
---
+++ database/seeds/StatusesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\models\Status;
+
+class StatusesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user_ids = ['1', '2', '3'];
+        $faker = app(Faker\Generator::class);
+
+        $stauses = factory(Status::class)->times(100)->make()->each(function ($status) use ($faker, $user_ids) {
+            $status->user_id = $faker->randomElement($user_ids);
+        });
+
+        Status::insert($stauses->toArray());
+    }
+}
