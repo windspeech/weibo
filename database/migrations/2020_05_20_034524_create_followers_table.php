0442d37b1c319dd1ea1e4542b0b6a05e6b6f5cb0
zhuozhanyang <windzhuo@goatgames.com>
2020-05-20 14:14:22 +0800

    粉丝数据表

---
+++ database/migrations/2020_05_20_034524_create_followers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->index()->comment('用户');
+            $table->integer('follower_id')->index()->comment('粉丝');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
